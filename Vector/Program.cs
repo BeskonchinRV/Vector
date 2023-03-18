da9098ce340e82bd775a51901281dfa64d8cac72
BeskonchinRV <112742675+BeskonchinRV@users.noreply.github.com>
2023-03-18 20:32:27 +0700

    Update Program.cs

--- Vector/Program.cs
+++ Vector/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+using System;
 
 namespace Vector
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vector vector = new Vector(3, 4);
+            Console.WriteLine(vector.ToString());
+            vector.X = 0;
+            vector.Y = -1;
+            Console.WriteLine(vector.ToString());
+            vector = new Vector(9, 40);
+            Console.WriteLine(vector.ToString());
+            Console.WriteLine(new Vector(0, 0).ToString());
         }
     }
 }
